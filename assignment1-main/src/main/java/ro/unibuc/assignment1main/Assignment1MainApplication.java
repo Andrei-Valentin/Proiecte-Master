eaaa835eb5b93b237c540642f6f9b7900dca6c03
Andrei-Valentin <51926009+Andrei-Valentin@users.noreply.github.com>
2021-12-03 00:05:08 +0200

    Add files via upload

---
+++ assignment1-main/src/main/java/ro/unibuc/assignment1main/Assignment1MainApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.assignment1main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1MainApplication.class, args);
+	}
+
+}
