be253d4b2926d2f007f9ed8f7bbd953bd2043525
Andrei-Valentin <51926009+Andrei-Valentin@users.noreply.github.com>
2022-01-27 22:44:05 +0200

    Create Trigger la nivel de comanda.sql

---
+++ Sisteme de baze de date/Trigger la nivel de comanda.sql
@@ -0,0 +1,29 @@
+--Trigger de tip LMD la nivel de comanda
+--Nu ne permite sa modificam salariul personalului in primele 3 luni ale anului
+
+CREATE OR REPLACE TRIGGER salariu_inceput_de_an
+    BEFORE UPDATE OF salariu  
+    ON personal
+DECLARE
+    luna_an NUMBER;
+BEGIN
+    luna_an := EXTRACT(MONTH FROM sysdate);
+    IF luna_an BETWEEN 1 AND 3 THEN
+        raise_application_error(-20200,'Salariul unui angajat nu poate fi modificat in primul trimestru al anului');
+    END IF;
+END;
+
+
+DECLARE
+exp_salariu exception;
+PRAGMA EXCEPTION_INIT(exp_salariu,-20200);
+
+BEGIN
+
+UPDATE PERSONAL
+SET SALARIU = SALARIU + 500;
+
+EXCEPTION 
+WHEN exp_salariu THEN
+DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLERRM(-20200)));
+END;
