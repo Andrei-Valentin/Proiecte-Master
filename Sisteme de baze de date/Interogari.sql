8bb539deae22046f38d4718cc50818eac12e14c6
Andrei-Valentin <51926009+Andrei-Valentin@users.noreply.github.com>
2022-01-27 20:42:12 +0200

    Update Interogari.sql

--- Sisteme de baze de date/Interogari.sql
+++ Sisteme de baze de date/Interogari.sql
@@ -5,3 +5,8 @@ SELECT sp.nume "Nume Spital", ad.nume_strada "Nume Strada", ad.oras "Oras", NVL(
 FROM spital sp JOIN adresa ad ON (sp.id_adresa = ad.id_adresa)
 ORDER BY ad.tara DESC, ad.oras ASC;
 
+
+--Afisarea personalului care a fost angajat in a 12-a zi a lunii
+SELECT nume "Nume", prenume "Prenume", data_angajare "Data Angajare"
+FROM personal
+WHERE personal.data_angajare LIKE ('12%');
